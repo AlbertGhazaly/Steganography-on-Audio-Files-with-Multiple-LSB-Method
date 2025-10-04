e18c8aa57a45e73a7609a000303e22b65de42e6a
Muhammad Rasheed Qais Tandjung <mrqaist@gmail.com>
2025-10-04 16:03:50 +0700

    feat: revise lsb algorithm

--- internal/stego/lsb.go
+++ internal/stego/lsb.go
@@ -51,33 +51,32 @@ func (l *LSBSteganography) addLengthHeader(message []byte) []byte {
 }
 
 func (l *LSBSteganography) embedData(carrier, message []byte, bits int) {
-
+	mask := byte((1 << bits) - 1)
 	totalMessageBits := len(message) * 8
+	carrierBitCapacity := len(carrier) * bits
 
-	for i := 0; i < totalMessageBits; i++ {
-		messageByteIndex := i / 8
-		messageBitPosition := 7 - (i % 8)
+	if totalMessageBits > carrierBitCapacity {
+		totalMessageBits = carrierBitCapacity
+	}
 
-		messageBit := (message[messageByteIndex] >> messageBitPosition) & 1
-
-		carrierByteIndex := (i * bits) / 8
-		if carrierByteIndex >= len(carrier) {
+	for i := 0; i < totalMessageBits; i += bits {
+		carrierIndex := i / bits
+		if carrierIndex >= len(carrier) {
 			break
 		}
 
-		carrierBitOffset := (i * bits) % 8
-		carrierBitPosition := 7 - carrierBitOffset
-
-		bitClearMask := byte(^(1 << carrierBitPosition))
-		carrier[carrierByteIndex] &= bitClearMask
-
-		if messageBit == 1 {
-			carrier[carrierByteIndex] |= (1 << carrierBitPosition)
+		messageBitStart := i
+		var chunk byte = 0
+		for b := 0; b < bits && messageBitStart+b < totalMessageBits; b++ {
+			msgByte := message[(messageBitStart+b)/8]
+			msgBitPos := 7 - ((messageBitStart + b) % 8)
+			msgBit := (msgByte >> msgBitPos) & 1
+			chunk = (chunk << 1) | msgBit
 		}
 
-		for j := 1; j < bits && carrierBitPosition-j >= 0; j++ {
-			carrier[carrierByteIndex] &= byte(^(1 << (carrierBitPosition - j)))
-		}
+		carrier[carrierIndex] &= ^mask
+
+		carrier[carrierIndex] |= chunk & mask
 	}
 }
 
@@ -85,123 +84,58 @@ func (l *LSBSteganography) ExtractMessage(mp3Data []byte, bits int) ([]byte, err
 	if bits < 1 || bits > 4 {
 		return nil, ErrInvalidBitCount
 	}
-
 	if len(mp3Data) <= l.headerSize {
 		return nil, ErrInvalidMP3Format
-	}
-
-	lengthBytes := make([]byte, 4)
-	for i := range lengthBytes {
-		lengthBytes[i] = 0
 	}
 
 	stegoData := mp3Data[l.headerSize:]
 	mask := byte((1 << bits) - 1)
 
-	for i := 0; i < 32; i++ {
-		byteIndex := i / 8
-		bitPosition := 7 - (i % 8)
+	lengthBits := 32
+	lengthBytes := make([]byte, 4)
 
-		carrierByteIndex := (i * bits) / 8
-		carrierBitOffset := (i * bits) % 8
-
-		var extractedBits byte
-		if carrierBitOffset+bits <= 8 {
-			extractedBits = (stegoData[carrierByteIndex] >> (8 - carrierBitOffset - bits)) & mask
-		} else {
-			bitsFromFirst := 8 - carrierBitOffset
-			extractedBits = (stegoData[carrierByteIndex] & ((1 << bitsFromFirst) - 1)) << (bits - bitsFromFirst)
-
-			if carrierByteIndex+1 < len(stegoData) {
-				bitsFromSecond := bits - bitsFromFirst
-				secondByteBits := (stegoData[carrierByteIndex+1] >> (8 - bitsFromSecond)) & ((1 << bitsFromSecond) - 1)
-				extractedBits |= secondByteBits
-			}
+	for i := 0; i < lengthBits; i += bits {
+		carrierIndex := i / bits
+		if carrierIndex >= len(stegoData) {
+			break
 		}
 
-		bit := (extractedBits >> (bits - 1)) & 1
+		bitChunk := stegoData[carrierIndex] & mask
 
-		if bit == 1 {
-			lengthBytes[byteIndex] |= (1 << bitPosition)
+		for b := 0; b < bits && i+b < lengthBits; b++ {
+			bitValue := (bitChunk >> (bits - 1 - b)) & 1
+			byteIndex := (i + b) / 8
+			bitPos := 7 - ((i + b) % 8)
+
+			if bitValue == 1 {
+				lengthBytes[byteIndex] |= (1 << bitPos)
+			}
 		}
 	}
 
 	messageLength := binary.BigEndian.Uint32(lengthBytes)
-
-	capacity := CalculateCapacity(len(mp3Data), bits)
-	if messageLength == 0 || int(messageLength) > capacity-4 {
-		return nil, ErrInvalidMP3Format
-	}
+	totalMessageBits := int(messageLength) * 8
 
 	message := make([]byte, messageLength)
-	for i := range message {
-		message[i] = 0
-	}
 
-	for i := 0; i < int(messageLength)*8; i++ {
-		byteIndex := i / 8
-		bitPosition := 7 - (i % 8)
-
-		carrierByteIndex := ((i + 32) * bits) / 8
-		carrierBitOffset := ((i + 32) * bits) % 8
-
-		if carrierByteIndex >= len(stegoData) {
+	for i := 0; i < totalMessageBits; i += bits {
+		carrierIndex := (i + lengthBits) / bits
+		if carrierIndex >= len(stegoData) {
 			break
 		}
 
-		var extractedBits byte
-		if carrierBitOffset+bits <= 8 {
-			extractedBits = (stegoData[carrierByteIndex] >> (8 - carrierBitOffset - bits)) & mask
-		} else {
-			bitsFromFirst := 8 - carrierBitOffset
-			extractedBits = (stegoData[carrierByteIndex] & ((1 << bitsFromFirst) - 1)) << (bits - bitsFromFirst)
+		bitChunk := stegoData[carrierIndex] & mask
 
-			if carrierByteIndex+1 < len(stegoData) {
-				bitsFromSecond := bits - bitsFromFirst
-				secondByteBits := (stegoData[carrierByteIndex+1] >> (8 - bitsFromSecond)) & ((1 << bitsFromSecond) - 1)
-				extractedBits |= secondByteBits
+		for b := 0; b < bits && i+b < totalMessageBits; b++ {
+			bitValue := (bitChunk >> (bits - 1 - b)) & 1
+			byteIndex := (i + b) / 8
+			bitPos := 7 - ((i + b) % 8)
+
+			if bitValue == 1 {
+				message[byteIndex] |= (1 << bitPos)
 			}
-		}
-
-		bit := (extractedBits >> (bits - 1)) & 1
-
-		if bit == 1 {
-			message[byteIndex] |= (1 << bitPosition)
 		}
 	}
 
 	return message, nil
-}
-
-func (l *LSBSteganography) extractData(carrier, output []byte, bits int) {
-	mask := byte((1 << bits) - 1)
-
-	for i := range output {
-		output[i] = 0
-	}
-
-	totalOutBits := len(output) * 8
-	currentOutBit := 0
-
-	for i := 0; i < len(carrier) && currentOutBit < totalOutBits; i++ {
-		extractedBits := carrier[i] & mask
-
-		outByteIndex := currentOutBit / 8
-		outBitOffset := currentOutBit % 8
-
-		if outBitOffset+bits <= 8 {
-			output[outByteIndex] |= extractedBits << (8 - outBitOffset - bits)
-		} else {
-			firstPartBits := 8 - outBitOffset
-			secondPartBits := bits - firstPartBits
-
-			output[outByteIndex] |= (extractedBits >> secondPartBits) & ((1 << firstPartBits) - 1)
-
-			if outByteIndex+1 < len(output) {
-				output[outByteIndex+1] |= (extractedBits & ((1 << secondPartBits) - 1)) << (8 - secondPartBits)
-			}
-		}
-
-		currentOutBit += bits
-	}
 }
